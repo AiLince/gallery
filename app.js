dfd592c69062df2547693c7c7eaf20faa39b3690
AiLince <116569614+AiLince@users.noreply.github.com>
2023-08-01 10:57:44 +0700

    fixed

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 2) {
+    const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
+    slides[activeSlide].classList.add('active')
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidesPlugin()
